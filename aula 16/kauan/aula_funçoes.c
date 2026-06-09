0508ac5754e07984c65c13f551949c2149700933
kauan silva <kauan.amado@edu.senai.br>
2026-06-09 16:59:03 -0300

    Add files via upload

---
+++ aula 16/kauan/aula_funçoes.c
@@ -0,0 +1,22 @@
+#include <stdio>
+
+void mostrarmensagem(){
+
+}
+
+
+
+
+
+int main(){
+printf("bem vindo ao programa!!!");
+
+}
+
+
+
+
+
+
+
+
---
+++ aula 16/kauan/funcoes_parte2.c
@@ -0,0 +1,50 @@
+#include <stdio>
+
+// voce pode passar informaçoes  para que a funçao 
+//realize uma tarefa , porn exemplo: voçe passar
+//2 numeros para ela REALIZAR A SOMA :
+//VC PASSA  ESTE PARAMETROS DETRO DOS PARAMETROS 
+
+// DECLARANDO UMA FUNÇAO QUE RECEBE 2 VALORES 
+vold soma (int num1 , int num2){
+   int total = num1 + num2;
+    printf(" o total da soma e:%d " , total);
+}
+// UNCOES COM RETORNO 
+// AO INVES DE SIMPLISMENTE MOSTRAR NA TELA O RESULTADO ,
+// A FUNCAO PODE ENTREGAR DE UM VALOR PARA UM  PROGRAMA 
+// VOCE PODE CAPTURAR ESTE RETORNO , POR EXEMPLO, GUARDANDO
+// ELE EM UMA VARIAVEL 
+
+//VAMOS FAZER UM CONVERSOS DE BYTES PARA BITS
+//SENDO QUE 1 BYTE = 8 BITS
+
+int  converterparabits(int valorembytes){
+    int valor em bits= valor em bytes
+}
+
+int main(){
+    soma(5,4); // chamando a funcao
+    soma(1000,1500);
+    soma(250,900);
+
+    //EXEMPLO UTILIZANDO VALORES INSERIDOS PELO USUARIO DO PROGRAMA 
+    int preco, frete;
+
+    printf("\ndigite o preco do produto: ");
+    scanf("%d" , &preco);
+
+    printf("\ndigite o valor do frete: ");
+    scanf("%d" , &frete);
+
+    soma(preco,frete);
+
+    printf("*** conversor de bytes para bits  ***");
+    int entradausuario);
+
+    int final;
+   final = converterparabits(entradausuario);
+   print("\n %d e o valor em bits", final
+
+}
+
